1ae689a1434b2cd90cbfbbeb37d94694e0172b1d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-09 14:07:17 +0530

    feat: add inquirer questions

--- app/cli.js
+++ app/cli.js
@@ -1,22 +1,28 @@
+const inquirer = require('inquirer');
+
 const validateArgsAndInputs = require('./utils/validate');
 const { flashError, showCLIVersion } = require('./utils/error');
+const questions = require('./utils/questions');
 
 const options = {};
 
 module.exports.options = options;
 
-module.exports = (_input, _options) => {
+module.exports = async (_input, _options) => {
 	const err = validateArgsAndInputs(_input, _options);
 
 	if (err) {
 		flashError(err);
 	}
 
-	const { version } = options;
+	const { version, repoName } = options;
 
 	if (version) {
 		showCLIVersion();
 	}
 
-	console.log(options);
+	if (repoName) {
+		const answers = await inquirer.prompt(questions);
+		console.log(answers);
+	}
 };
---
+++ app/utils/questions.js
@@ -0,0 +1,34 @@
+const questions = [
+	{
+		name: 'description',
+		type: 'input',
+		message: 'Enter a short description of the repository:',
+	},
+	{
+		name: 'isPrivate',
+		type: 'confirm',
+		message: 'Do you want to create a private repository?',
+	},
+	{
+		name: 'hasIssues',
+		type: 'confirm',
+		message: 'Do you want to enable issues for this repository?',
+	},
+	{
+		name: 'hasProjects',
+		type: 'confirm',
+		message: 'Do you want to enable projects for this repository?',
+	},
+	{
+		name: 'hasWiki',
+		type: 'confirm',
+		message: 'Do you want to enable the wiki for this repository?',
+	},
+	{
+		name: 'token',
+		type: 'input',
+		message: 'Enter GitHub token:',
+	},
+];
+
+module.exports = questions;
