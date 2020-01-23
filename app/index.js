a2edbe56874c07ab1df331da49969aff3906f757
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-23 20:15:07 +0530

    fix: cli input flag option

--- app/index.js
+++ app/index.js
@@ -14,16 +14,15 @@ const cli = meow(
 
 	Options
 		-v, --version          Show the version and exit with code 0
-	
+
 	Examples
 		$ create-remote-repo test-repo
 `,
 	{
 		flags: {
-			boolean: ['version'],
-			string: [],
-			alias: {
-				v: 'version',
+			version: {
+				type: 'boolean',
+				alias: 'v',
 			},
 		},
 	}
